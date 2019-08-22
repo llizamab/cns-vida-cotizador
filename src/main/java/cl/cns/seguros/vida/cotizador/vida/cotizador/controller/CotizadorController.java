a189d8e2792f80c19506739a29e0289b36eb25e9
llizamab <30680703+llizamab@users.noreply.github.com>
2019-08-22 18:37:37 -0400

    Update CotizadorController.java

--- src/main/java/cl/cns/seguros/vida/cotizador/vida/cotizador/controller/CotizadorController.java
+++ src/main/java/cl/cns/seguros/vida/cotizador/vida/cotizador/controller/CotizadorController.java
@@ -18,7 +18,7 @@ import java.net.InetAddress;
 @RestController
 public class CotizadorController {
 
-    private static final String template = "Hello, from %s!";
+    private static final String template = "Hello, from %s! v2";
 
     private final AtomicLong counter = new AtomicLong();
 
